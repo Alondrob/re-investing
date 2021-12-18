52bbca303881acda0565cf870918b3e767385197
Alondrob <alondrob@gmail.com>
2021-12-17 22:43:21 -0500

    12.17

--- re-investing-backend/db/seeds.rb
+++ re-investing-backend/db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'uri'
+require 'net/http'
+require 'openssl'
+
+url = URI("https://realty-mole-property-api.p.rapidapi.com/saleListings?city=Austin&state=TX")
+
+http = Net::HTTP.new(url.host, url.port)
+http.use_ssl = true
+http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+
+request = Net::HTTP::Get.new(url)
+request["x-rapidapi-host"] = 'realty-mole-property-api.p.rapidapi.com'
+request["x-rapidapi-key"] = '4f49e44fe5mshb356fb96b2bc15ap109f78jsne88cc1e9d543'
+
+response = http.request(request)
+puts response.read_body
